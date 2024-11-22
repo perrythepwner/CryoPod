af5efcff11d50c8dc879435d10b46616aa85d9a5
simonedimaria <117766011+perrythepwner@users.noreply.github.com>
2024-11-23 00:13:59 +0100

    added frontend (blockscout)

--- build-docker.sh
+++ build-docker.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 set -ex
-
+set -o allexport
 ########### ENV VARS ###########
-NAME=chainscout
+NAME=cryopod
 IMAGE=blockchain_${NAME}
 FLAG=HTB{pl4c3h0ld3r}
 PUBLIC_IP=127.0.0.1
 HANDLER_PORT=8000
 LOCAL_RPC_PORT=5000
+LOCAL_RPC_URL=http://localhost:${LOCAL_RPC_PORT}/
 PUBLIC_RPC_PORT=8888
 FRONTEND_PORT=8080
-ANVIL_LOGFILE=anvil_output.log
+envsubst < .env.blockscout.template > .env.blockscout.evaluated
 ################################
+set +o allexport
 
 docker rm -f $IMAGE
 docker build --tag=$IMAGE:latest ./challenge/ && \
@@ -21,10 +23,11 @@ docker run --rm -it \
     -p "$FRONTEND_PORT:$FRONTEND_PORT" \
     -p "$HANDLER_PORT:$HANDLER_PORT" \
     -e PUBLIC_IP=$PUBLIC_IP \
+    -e HANDLER_PORT=$HANDLER_PORT \
     -e PUBLIC_RPC_PORT=$PUBLIC_RPC_PORT \
     -e LOCAL_RPC_PORT=$LOCAL_RPC_PORT \
-    -e HANDLER_PORT=$HANDLER_PORT \
-    -e ANVIL_LOGFILE=$ANVIL_LOGFILE \
+    -e LOCAL_RPC_URL=$LOCAL_RPC_URL \
     -e FLAG=$FLAG \
+    --env-file .env.blockscout.evaluated \
     --name $IMAGE \
     $IMAGE:latest
